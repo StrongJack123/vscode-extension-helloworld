8e1eb7584fba62d0633389036005cb3117e4fb37
StrongJack123 <greatwisdom1223@gmail.com>
2022-10-01 09:36:28 +0900

    change the extension message

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('heloworld.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Helloworld!');
+		vscode.window.showInformationMessage('This is my first extension - Congratulation - Hello WORLD');
 	});
 
 	context.subscriptions.push(disposable);
